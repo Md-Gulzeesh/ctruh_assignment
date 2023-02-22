31a3073ffad7e1fdf03b79ee0f3bb6e5f5e41435
Md-Gulzeesh <gulzeesh2000@gmail.com>
2023-02-22 19:05:34 +0530

    color picker and canvas resizable functionality added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,39 @@ import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [colorValue, setColorValue] = useState('');
+  const [colorValue, setColorValue] = useState("#000000");
   const handleChange = (e)=>{
       setColorValue(e.target.value);
   }
   return (
     <div className="container">
-      <div className='color_picker_container'>
-        <input  onChange={handleChange} type="color" name="colorPicker" id="colorPicker" />
+      <div className="color_picker_container">
+        <div>
+          <input
+            onChange={handleChange}
+            type="color"
+            name="colorPicker"
+            id="colorPicker"
+          />
+          <span>{colorValue}</span>
+        </div>
         <select name="" id="">
+          <option value="">Choose color palette</option>
+          <option value="">Choose color palette</option>
           <option value="">Choose color palette</option>
         </select>
       </div>
-      <canvas style={{ border: "1px solid red",background:colorValue }}></canvas>
+      <div style={{ resize: "both", overflow: "hidden", width: "60%" }}>
+        <canvas
+          style={{
+            border: "1px solid gainsboro",
+            borderRadius: "12px",
+            background: colorValue,
+            width: "100%",
+            height: "100%",
+          }}
+        ></canvas>
+      </div>
     </div>
   );
 }
