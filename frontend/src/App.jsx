dc8cbf90a3110f9552f50fa7ab6d4fae5c2ffd3a
Md-Gulzeesh <gulzeesh2000@gmail.com>
2023-02-22 17:53:38 +0530

    canvas and color picker added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+  const [colorValue, setColorValue] = useState('');
+  const handleChange = (e)=>{
+      setColorValue(e.target.value);
+  }
   return (
-    <></>
-  )
+    <div className="container">
+      <div className='color_picker_container'>
+        <input  onChange={handleChange} type="color" name="colorPicker" id="colorPicker" />
+        <select name="" id="">
+          <option value="">Choose color palette</option>
+        </select>
+      </div>
+      <canvas style={{ border: "1px solid red",background:colorValue }}></canvas>
+    </div>
+  );
 }
 
 export default App
